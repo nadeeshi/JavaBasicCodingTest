fa2cd009bbcc4f24178c8513d55dffb007243a11
Nadeeshika senavirathna <nadeesenavirathna@gmail.com>
2022-12-02 13:16:51 +0100

    add strings problems and remove number bin folder

---
+++ src/se/com/nadee/sample/strings/ReverseStringInputScanner.java
@@ -0,0 +1,21 @@
+package se.com.nadee.sample.strings;
+import java.util.Scanner;
+
+public class ReverseStringInputScanner {
+	
+	public static void main(String args[]) {
+		String str;
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter your String");
+		
+		str = input.nextLine();
+		
+		String[] token = str.split("");
+		
+		for(int i = token.length - 1; i>=0; i--) {
+			System.out.print(token[i]+"");
+		}
+	}
+
+}
---
+++ src/se/com/nadee/sample/strings/WordReversal.java
@@ -0,0 +1,10 @@
+package se.com.nadee.sample.strings;
+
+public class WordReversal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
